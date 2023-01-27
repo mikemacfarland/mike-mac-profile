0ca2d70b3a4bdba7e158fc4d89c98ff23d92ce39
mike macfarland <m_macfarland@live.com>
2023-01-26 19:32:37 -0500

    added portfolio projects section, added js for section, added imgas for projects

--- src/app.js
+++ src/app.js
@@ -16,9 +16,10 @@ const scrollItems = [
 const scrollDisable = false
 
 // sections
-const about = document.querySelector('#about')
-const skills = document.querySelector('#skills')
+const about = document.querySelector('.section_about')
+const skills = document.querySelector('.section_skills')
 const contact = document.querySelector('#contact')
+const projects = document.querySelector('.section_projects')
 
 // elements
 const nameHeading = document.querySelector('#h1-1')
@@ -31,57 +32,88 @@ const techList = about.querySelector('.tech_list')
 const contactLink = document.querySelector('.contact_link')
 const sliders = document.querySelectorAll('.slider')
 const skillsList = skills.querySelectorAll('li')
+const portraitProjs = projects.querySelectorAll('.portrait')
+const squareProjs = projects.querySelectorAll('.square')
 
 // initial opacity for elements in hero section
 setTimeout(()=>{nameHeading.className='show'},300)
 setTimeout(()=>{titleHeading.className='show'},700)
 setTimeout(()=>{contactLink.classList.add('show')},1100)
 
-// event listener for scrolling
+// event listener for scroll
 window.addEventListener('scroll',()=>{
 
-const getPos = (el)=>{
-    const scrollData ={
-        scrollHeight:document.body.scrollHeight,
-        windowHeight:window.innerHeight,
-        windowTop:window.scrollY,
-        elementBottom:window.innerHeight - el.getBoundingClientRect().bottom,
-        elementTop:el.getBoundingClientRect().top,
-        windowBottom:window.scrollY + window.innerHeight
-    }
+
+const getPos = (el,pos)=>{
+    // const scrollData ={
+    //     // scrollHeight:document.body.scrollHeight,
+    //     // windowHeight:window.innerHeight,
+    //     // windowBottom:window.scrollY + window.innerHeight,
+    //     // windowTop:window.scrollY,
+    //     elementTop:el.getBoundingClientRect().top,
+    //     elementBottom:window.innerHeight - el.getBoundingClientRect().bottom
+    // }
+
+    // TOP is determined from top of viewport to top of element 
+    // CENTER is determined from bottom of viewport to center of element
+    // BOTTOM is determined from bottom of viewport to bottom of element
+
+    // @TODO can add logic to determine distance as well ?
+    const scrollData = pos === 'top' ? el.getBoundingClientRect().top : pos === 'center' ?  -(el.getBoundingClientRect().bottom - (el.getBoundingClientRect().height / 2) - window.innerHeight) : window.innerHeight - el.getBoundingClientRect().bottom
     return scrollData
 }
 
     // conditionals and expressions for setting element styles/classes based on scroll position
-    getPos(nameHeading).elementTop < 120 ? nameHeading.className = 'hidden-left' : nameHeading.className = 'show'
-    getPos(titleHeading).elementTop < 80 ? titleHeading.className = 'hidden-right' : titleHeading.className = 'show'
-    getPos(contactLink).elementTop < 100 ? contactLink.className = ('hidden') : contactLink.className = ('show')
-    if(getPos(aboutHeading).elementBottom > 100){
+    getPos(nameHeading,'top') < 120 ? nameHeading.className = 'hidden-left' : nameHeading.className = 'show'
+    getPos(titleHeading,'top') < 80 ? titleHeading.className = 'hidden-right' : titleHeading.className = 'show'
+    getPos(contactLink,'top') < 100 ? contactLink.className = ('hidden') : contactLink.className = ('show')
+    if(getPos(aboutHeading) > 100){
         aboutHeading.classList.add('show')
         setTimeout(()=>{aboutP.forEach((p)=>p.classList.add('show'))},400)
         setTimeout(()=>{techList.classList.add('show')},800)
     }
-    getPos(skillsHeading).elementBottom > 80 ? skillsHeading.className = 'show' : skillsHeading.className = 'hidden'
-    getPos(contactHeading).elementBottom > 100 ? contactHeading.className = 'show' : contactHeading.className = 'hidden-left'
-    // if there are two contact links use this with a indexer
-    // getPos(contactLink).elementBottom > 120 ? contactLink.className = 'show' : contactLink.className = 'hidden-right'
 
-    // for screens smaller than 767px
-    if(window.innerWidth < 1023){
-        getPos(skillsList[0]).elementBottom > 80 ? skillsList[0].className = 'show' : skillsList[0].className = 'hidden'
-        getPos(skillsList[1]).elementBottom > 80 ? skillsList[1].className = 'show' : skillsList[1].className = 'hidden'
-        getPos(skillsList[2]).elementBottom > 80 ? skillsList[2].className = 'show' : skillsList[2].className = 'hidden'
+    // project section
+    if(getPos(projects.querySelector('h1')) > 100) projects.querySelector('h1').className = 'show'
+    if(getPos(projects.querySelector('h2')) > 100) projects.querySelector('h2').className = 'show'
+    if(getPos(projects.querySelector('p')) > 100) projects.querySelector('p').className = 'show'
+    
+
+    // project 
+    if(getPos(portraitProjs[0],'center') > 250){
+        portraitProjs[0].classList.add('scaleup')
+        setTimeout(()=>{portraitProjs[1].classList.add('scaleup')},200)
+        setTimeout(()=>{squareProjs[1].classList.add('scaleup')},650)
+        setTimeout(()=>{squareProjs[0].classList.add('scaleup')},850)
     }
+
+    // skills section heading
+    getPos(skillsHeading) > 80 ? skillsHeading.className = 'show' : skillsHeading.className = 'hidden'
+    
+    // for screens smaller than < 1023px
+    if(window.innerWidth < 1023){
+        getPos(skillsList[0]) > 80 ? skillsList[0].className = 'show' : skillsList[0].className = 'hidden'
+        getPos(skillsList[1]) > 80 ? skillsList[1].className = 'show' : skillsList[1].className = 'hidden'
+        getPos(skillsList[2]) > 80 ? skillsList[2].className = 'show' : skillsList[2].className = 'hidden'
+    }
+
+    // contact section
+    getPos(contactHeading) > 100 ? contactHeading.className = 'show' : contactHeading.className = 'hidden-left'
+    // if there are two contact links use this with a indexer
+    // getPos(contactLink) > 120 ? contactLink.className = 'show' : contactLink.className = 'hidden-right'
 
     // for screens larger than than 1023px
     if(window.innerWidth > 1023){
-        getPos(skillsList[0]).elementBottom > 80 ? skillsList[0].className = 'show' : skillsList[0].className = 'hidden'
-        getPos(skillsList[1]).elementBottom > 180 ? skillsList[1].className = 'show' : skillsList[1].className = 'hidden'
-        getPos(skillsList[2]).elementBottom > 280 ? skillsList[2].className = 'show' : skillsList[2].className = 'hidden'
+        getPos(skillsList[0]) > 80 ? skillsList[0].className = 'show' : skillsList[0].className = 'hidden'
+        getPos(skillsList[1]) > 180 ? skillsList[1].className = 'show' : skillsList[1].className = 'hidden'
+        getPos(skillsList[2]) > 280 ? skillsList[2].className = 'show' : skillsList[2].className = 'hidden'
     }
 
 })
 
+
+
+// bullet for about section list
 const Bullet =(item)=> {
     return (
         `<li class='bullet'>
@@ -91,12 +123,14 @@ const Bullet =(item)=> {
     )
 }
 
+// scroll itrem for marquee
 const ScrollItem = (item)=> {
     return (
     `<div class='_item' ><p>*</p>${item.name}</div>`
     )
 }
 
+// set scroll and marquee items
 scrollItems.map((item)=>{
     techList.innerHTML += Bullet(item)
     sliders.forEach((slider)=>{
